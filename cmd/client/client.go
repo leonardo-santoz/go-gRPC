f531166c584bdbcff53d9ba728c283ae9eec6ae5
leonardo-santoz <leonardo.cienciacomputer@gmail.com>
2021-02-20 18:22:04 -0300

    implementando nosso primeiro client

---
+++ cmd/client/client.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/leonardo-santoz/go-gRPC/pb"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	connection, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Could not coneect to gRPC Server: %v", err)
+	}
+	defer connection.Close()
+
+	client := pb.NewUserServiceClient(connection)
+	AddUser(client)
+}
+
+func AddUser(client pb.UserServiceClient) {
+	req := &pb.User{
+		Id:    "0",
+		Name:  "Joao",
+		Email: "j@j.com",
+	}
+
+	res, err := client.AddUser(context.Background(), req)
+
+	if err != nil {
+		log.Fatalf("Could not make gRPC request: %v", err)
+	}
+
+	fmt.Println(res)
+}
